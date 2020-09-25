a0e6d38b1b5bb82b7fc15a5139be21a2c855538e
Pertim <mobanbox@gmail.com>
2020-09-26 06:29:37 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -600,6 +600,7 @@ Debian_apt(){
 # 下载 ShadowsocksR
 Download_SSR(){
 	cd "/usr/local/"
+	rm -rf shadowsocksr
 	[[ ! -e "manyuser.zip" ]] && echo -e "${Error} ShadowsocksR服务端 压缩包 下载失败 !" && exit 1
 	unzip "manyuser.zip"
 	[[ ! -e "/usr/local/shadowsocksr-manyuser/" ]] && echo -e "${Error} ShadowsocksR服务端 解压失败 !" && rm -rf manyuser.zip && exit 1
